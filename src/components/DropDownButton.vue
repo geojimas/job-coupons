3af74ba27cc1e5e7200b88df0a65e433a0844031
geojimas <geojimas@gmail.com>
2023-10-20 01:45:50 +0300

    init coupons app

---
+++ src/components/DropDownButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-md">
+    <q-btn-dropdown color="pink" label="kjhb" dropdown-icon="change_history">
+      <div class="row no-wrap q-pa-md">
+        <div class="column">
+          <div class="text-h6 q-mb-md">Settings</div>
+          <p>Profile</p>
+        </div>
+
+        <q-separator vertical inset class="q-mx-lg" />
+
+        <div class="column items-center">
+          <q-avatar size="72px">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+
+          <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+          <q-btn color="primary" label="Logout" push size="sm" v-close-popup />
+        </div>
+      </div>
+    </q-btn-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      onItemClick() {
+        console.log('Clicked on an Item')
+      }
+    }
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Coupons
+        </q-toolbar-title>
+        <InfoPopup />
+        <DropDownButton />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+import DropDownButton from 'src/components/DropDownButton.vue'
+import InfoPopup from 'src/components/InfoPopup.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+    DropDownButton,
+    InfoPopup
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
